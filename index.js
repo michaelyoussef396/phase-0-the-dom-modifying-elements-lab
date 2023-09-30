58e816e13330809cf13d818c5803b9f055c6e6d8
michael <michaelyoussef396@gmail.com>
2023-10-01 05:25:11 +1100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent = 'Michael is the champion';
+
+document.body.appendChild(newHeader);
